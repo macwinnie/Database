a6aa21856bc64817017c2a7b9c8cfa0ae03dfe7b
Martin Winter <mail@winter-martin.de>
2012-12-28 23:02:12 +0100

    working without config.php

--- Database.class.php
+++ Database.class.php
@@ -1,74 +1,57 @@
 <?php
 class Database{
-    private $mysqli;
-    private $connect_errno;
-    private $connect_error;
-    private $errno;
-    private $error;
-    private $cache;
-    private $count;
-    private $hostname;
-    private $lastquerysql;
-    
-    public function __construct(){
+	private $mysqli;
+	private $connect_errno;
+	private $connect_error;
+	private $errno;
+	private $error;
+	private $count;
+	private $prefix;
+	private $hostname;
+	private $lastQuery;
+	
+	public function __construct($host=NULL, $database=NULL, $user=NULL, $pass=NULL, $prefix=NULL){
         $this->hostname = php_uname('n');
-        $this->cache = array();
-        require_once('config.php');
-       	@$this->mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-        if(!@$this->mysqli->stat()){
-            echo json_encode(array("data" => array("errno" => $this->mysqli->connect_errno, "error" => $this->mysqli->connect_error), "error" => 2));
-            $this->connect_errno = null;
-            $this->connect_error = $this->mysqli->connect_error;
-            exit;
-        }
-        if ($this->mysqli->connect_errno || !$this->mysqli) {
+	    @$this->mysqli = new mysqli($host, $user, $pass, $database);
+	    if(!@$this->mysqli->stat()){
+			echo json_encode(array("data" => array("errno" => $this->mysqli->connect_errno, "error" => $this->mysqli->connect_error), "error" => 2));
+			$this->connect_errno = null;
+			$this->connect_error = $this->mysqli->connect_error;
+			exit;
+		}
+		if ($this->mysqli->connect_errno || !$this->mysqli) {
             echo json_encode(array("data" => array("errno" => $this->mysqli->connect_errno, "error" => $this->mysqli->connect_error), "error" => 2));
             $this->connect_errno = $this->mysqli->connect_errno;
             $this->connect_error = $this->mysqli->connect_error;
             exit;
         }
-    }
-    
-    public function query($query, $params = array()){
-        $sql = $this->prepare_query($query, $params);
-        $result = $this->mysqli->query($sql);
-        if(!is_null($result) && $this->mysqli->errno == 0){
-            $this->errno = null;
-            $this->error = null;
-            $data_arr = array();
-            $this->count = $result->num_rows;
-            while ($row = $result->fetch_assoc()) {
-                $data_arr[] = $row;
-            }
-            $result->close();
-            return $data_arr;
-        }else{
-            $this->errno = $this->mysqli->errno;
-            $this->error = $this->mysqli->error;
-            return false;
-        }
-    }
-    
-    /*public function query_scalar($query, $params = array()){
-    	$sql = $this->prepare_query($query, $params);
-    	$result = $this->cacheout($sql, "scalar");
-        if($result !== false){
-            return $result;
-        }elseif($result == false && $result = $this->mysqli->query($sql)){
-            $this->errno = null;
-            $this->error = null;
-            $row = $result->fetch_array();
-            $this->cachein($sql, "scalar", $row[0]);
-            $result->close();
-            return $row[0];
-        }else{
-            $this->errno = $this->mysqli->errno;
-            $this->error = $this->mysqli->error;
-            return false;
-        }
-    }*/
-    
-    public function query_scalar($query, $params = array()){
+		$this->connect_errno = null;
+		$this->connect_error = null;
+		$this->prefix = $prefix;
+		return true;
+	}
+	
+	public function query($query, $params = array()){
+	    $sql = $this->prepare_query($query, $params);
+	    $result = $this->mysqli->query($sql);
+	    if(!is_null($result) && $this->mysqli->errno == 0){
+	        $this->errno = null;
+	        $this->error = null;
+	        $data_arr = array();
+	        $this->count = $result->num_rows;
+	        while ($row = $result->fetch_assoc()) {
+	            $data_arr[] = $row;
+	        }
+	        $result->close();
+	        return $data_arr;
+	    }else{
+	        $this->errno = $this->mysqli->errno;
+	        $this->error = $this->mysqli->error;
+	        return false;
+	    }
+	}
+	
+	public function query_scalar($query, $params = array()){
         $row = $this->query_row($query, $params);
         if($row === false){
             return false;
@@ -76,123 +59,111 @@ class Database{
             return $row[0];
         }
     }
-    
-    public function query_row($query, $params = array()){
-    	$sql = $this->prepare_query($query, $params);
-    	$result = $this->cacheout($sql, "row");
-        if($result !== false){
-            return $result;
-        }elseif($result == false && $result = $this->mysqli->query($sql)){
-            $this->errno = null;
-            $this->error = null;
-            $row = $result->fetch_array();
-            $this->cachein($sql, "row", $row);
-            $result->close();
-            return $row;
-        }else{
-            $this->errno = $this->mysqli->errno;
-            $this->error = $this->mysqli->error;
-            return false;
-        }
-    }
-    
-    public function execute($query, $params = array()){
-        $sql = $this->prepare_query($query, $params);
-        if($this->mysqli->query($sql)){
-            $this->errno = null;
-            $this->error = null;
-            return true;
-        }else{
-            $this->errno = $this->mysqli->errno;
-            $this->error = $this->mysqli->error;
-            return false;
-        }
-    }
-    
-    public function geterrno(){
-        return $this->errno;
-    }
-    
-    public function geterror(){
-        return $this->error;
-    }
-    
-    public function getconnerrno(){
-        return $this->connect_errno;
-    }
-    
-    public function getconnerror(){
-        return $this->connect_error;
-    }
-    
-    private function prepare_query($query, $params){
-    	global $dbprefix;
-    	if(substr($query, -1) != ";"){
+	
+	public function query_row($query, $params = array(), $assoc = true){
+		$sql = $this->prepare_query($query, $params);
+	    if($result !== false){
+	        return $result;
+	    }elseif($result == false && $result = $this->mysqli->query($sql)){
+	        $this->errno = null;
+	        $this->error = null;
+	        if ($assoc) {
+	            $row = $result->fetch_assoc();
+	        }
+	        else {
+		        $row = $result->fetch_array();
+	        }
+	        $result->close();
+	        return $row;
+	    }else{
+	        $this->errno = $this->mysqli->errno;
+	        $this->error = $this->mysqli->error;
+	        return false;
+	    }
+	}
+	
+	public function execute($query, $params = array()){
+	    $sql = $this->prepare_query($query, $params);
+	    if($this->mysqli->query($sql)){
+	        $this->errno = null;
+	        $this->error = null;
+	        return true;
+	    }else{
+	        $this->errno = $this->mysqli->errno;
+	        $this->error = $this->mysqli->error;
+	        return false;
+	    }
+	}
+	
+	public function geterrno(){
+	    return $this->errno;
+	}
+	
+	public function geterror(){
+	    return $this->error;
+	}
+	
+	public function getconnerrno(){
+	    return $this->connect_errno;
+	}
+	
+	public function getconnerror(){
+	    return $this->connect_error;
+	}
+	
+	private function prepare_query($query, $params){
+		if(substr(trim($query), -1) != ";"){
     		$query .= ";";
-    	};
-    	foreach($params as $key => $value){
-    		if(is_array($value)){
-    			$i=0;
-    			foreach($value as $subval){
-    				$value[$i] = $this->mysqli->real_escape_string($subval);
-    				if(!is_int($value[$i])){
-    					$value[$i] = "\"".$value[$i]."\"";
-    				}
-    				$i++;
-    			}
-    			$value = implode(", ", $value);
-    		}else{
-    			$value = $this->mysqli->real_escape_string($value);
-    			if(!is_int($value)){
-    				$value = "\"".$value."\"";
-    			}
-    		}
-    		$query = preg_replace('#'.$key.'\b#', $value, $query);
     	}
-    	$query = preg_replace("/\s\{(\w*)\}(\s|;|)/", " `".$dbprefix."\\1`\\2", $query);
-    	$this->lastquerysql = $query;
-    	return $query;
-    }
-    
-    private function cachein($sql, $type, $result){
-        $this->cache[$sql][$type] = $result;
-    }
-    
-    private function cacheout($sql, $type){
-        if(array_key_exists($sql, $this->cache) && array_key_exists($type, $this->cache[$sql]) && !is_null($this->cache[$sql][$type])){
-            $result = $this->cache[$sql][$type];
-        }else{
-            return false;
-        }
-        return $result;
-    }
-    
-    public function getinfo(){
+		foreach($params as $key => $value){
+			if(is_array($value)){
+				$i=0;
+				foreach($value as $subval){
+					$value[$i] = $this->mysqli->real_escape_string($subval);
+					if(!is_int($value[$i])){
+						$value[$i] = "\"".$value[$i]."\"";
+					}
+					$i++;
+				}
+				$value = implode(", ", $value);
+			}else{
+				$value = $this->mysqli->real_escape_string($value);
+				if(!is_int($value)){
+					$value = "\"".$value."\"";
+				}
+			}
+			$query = preg_replace('#' . $key . '\b#', $value, $query);
+		}
+		$query = preg_replace("/\s\{(\w*)\}(\s|;|)/", " `".$this->prefix."\\1`\\2", $query);
+		$this->lastQuery = $query;
+		return $query;
+	}
+	
+	public function getinfo(){
         return array("server" => array("stat" => $this->mysqli->stat, "version" => $this->mysqli->server_info, "info" => $this->mysqli->host_info, "thread_id" => $this->mysqli->thread_id), "connection" => array("sqlstate" => $this->mysqli->sqlstate, "protocol_versoin" => $this->mysqli->protocol_version), "client" => array("version" => $this->mysqli->client_version, "info" => $this->mysqli->client_info));
     }
-    
-    public function getlastid(){
-        return $this->mysqli->insert_id;
-    }
-    
-    public function getlastcount(){
-        return $this->count;
-    }
-    
-    public function getaffectedrows(){
+	
+	public function getlastid() {
+	    return $this->mysqli->insert_id;
+	}
+	
+	public function getcount() {
+	    return $this->count;
+	}
+	
+	public function getaffectedrows(){
         return $this->mysqli->affected_rows;
     }
-    
-    public function getfieldcount(){
-        return $this->mysqli->field_count;
-    }
-    
-    public function getlastquerysql(){
-    	return $this->lastquerysql();
-    }
-    
-    public function __destruct(){
-        @$this->mysqli->close();
-        unset($this->cache);
-    }
+	
+	public function getfieldcount() {
+	    return $this->mysqli->field_count;
+	}
+	
+	public function getLastQuery() {
+		return $this->lastQuery;
+	}
+	
+	public function __destruct(){
+	    $this->mysqli->close();
+	}
 }
