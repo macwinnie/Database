d11db5fcb3983dbfe64e88462e66013b3fd4c7e8
tsia <accounts@tsia.de>
2013-02-03 22:19:33 +0100

    added database connection config to instantiation

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once("Database.class.php");
-$db = new Database;
+require_once("config.php");
+$db = new Database($dbhost, $dbname, $dbuser, $dbpass, $dbprefix);
 if(intval($db->getconnerrno) != 0 || intval($db->getconnerror) != 0){
 	echo $db->getconnerrno().": ".$db->getconnerror();
 	exit;
