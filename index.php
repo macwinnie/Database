4ae99966753e0bfe0f971524b9a281d6e196feec
Martin Winter <mail@winter-martin.de>
2013-02-24 19:14:38 +0100

    Example-BugFix

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once("Database.class.php");
 require_once("config.php");
 $db = new Database($dbhost, $dbname, $dbuser, $dbpass, $dbprefix);
-if(intval($db->getconnerrno) != 0 || intval($db->getconnerror) != 0){
+if(intval($db->getconnerrno()) != 0 || intval($db->getconnerror()) != 0){
 	echo $db->getconnerrno().": ".$db->getconnerror();
 	exit;
 }
