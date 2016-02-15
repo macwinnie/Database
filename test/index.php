0522df715f63f1ec7307153ee61889dde14bfb57
Martin Winter <dev@macwinnie.me>
2016-02-15 18:02:56 +0100

    Update index.php

--- test/index.php
+++ test/index.php
@@ -11,12 +11,3 @@ try {
 } catch (Exception $e) {
 	echo $e->getMessage();
 }
-
-try {
-	$sql = 'SELECT * FROM {table1} WHERE `id` = :i';
-	echo '<pre>'.print_r($db->query($sql, array(':i'=>1)),true).'</pre>';
-
-	echo '<pre>'.print_r($db->describe(),true).'</pre>';
-} catch (\Exception $e) {
-	echo '<h3>Exception</h3>'.$e->getMessage();
-}
